d937b6e21d5d5c1e913290af8f9ceb8670f0972d
hanchang97 <nimok97@gmail.com>
2022-01-22 03:25:49 +0900

    c++/ BOJ 18258

---
+++ 백준/바킹독 문제집/큐/18258.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <queue>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+
+	queue<int> Q;
+
+	for (int i = 0; i < N; i++) {
+		string command;
+		cin >> command;
+
+		if (command == "push") {
+			int x;
+			cin >> x;
+			Q.push(x);
+		}
+		else if (command == "pop") {
+			if (Q.empty())
+				cout << "-1\n";
+			else {
+				cout << Q.front() << "\n";
+				Q.pop();
+			}
+		}
+		else if (command == "size") {
+			cout << Q.size() << "\n";
+		}
+		else if (command == "empty") {
+			if (Q.empty())
+				cout << "1\n";
+			else
+				cout << "0\n";
+		}
+		else if (command == "front") {
+			if (Q.empty())
+				cout << "-1\n";
+			else
+				cout << Q.front() << "\n";
+		}
+		else if (command == "back") {
+			if (Q.empty())
+				cout << "-1\n";
+			else
+				cout << Q.back() << "\n";
+		}
+
+	}
+
+	return 0;
+}
