76b19f49d547e32deddd954cae5aa884ba82741a
hanchang97 <nimok97@gmail.com>
2022-05-10 01:42:44 +0900

    [수정] C++/ BOJ - 1931 silver1

--- 백준/바킹독 문제집/그리디/1931.cpp
+++ 백준/바킹독 문제집/그리디/1931.cpp
@@ -1,97 +1,58 @@
-#include <iostream>
-#include <string>
-#include <deque>
+#include<iostream>
 #include <vector>
-#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
+int N;
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+struct Data {
+	int start;
+	int end;
+};
 
-	int T;
-	cin >> T;
-
-	while (T--) {
-		string command; // 명령어 입력
-		cin >> command;
-
-		int count; // 배열에 들어가 있는 정수 개수
-		cin >> count;
-
-		string countStr; // 배열에 들어가 있는 정수 문자열
-		cin >> countStr;
-
-		deque<string> dq;  // 문자열 분할 후 담을 덱
-
-		countStr.erase(countStr.length() - 1, 1);
-		countStr.erase(0, 1);
-
-		stringstream ss(countStr);
-		string token;
-
-		while (getline(ss, token, ',')) {
-			dq.push_back(token);
-			//cout << "token : " << token << "\n";
-		}
-
-
-		bool frontIsLeft = true;
-		bool isError = false;
-
-		for (int i = 0; i < command.length(); i++) {
-			if (command[i] == 'R') {
-				frontIsLeft = !frontIsLeft;  // R 명령을 O(1) 로 처리
-			}
-			else {
-				if (dq.size() == 0) {
-					isError = true;
-					break;
-				}
-				else {
-					if (frontIsLeft)
-						dq.pop_front();
-					else
-						dq.pop_back();
-				}
-			}
-		}
-
-		if (isError)
-			cout << "error\n";
-		else {
-			cout << "[";
-			if (frontIsLeft) {
-				for (int t = 0; t < dq.size(); t++) {
-					if (t == 0)
-						cout << dq[t];
-					else
-						cout << "," << dq[t];
-				}
-			}
-			else {
-				for (int t = dq.size() - 1; t >= 0; t--) {
-					if (t == dq.size() - 1)
-						cout << dq[t];
-					else
-						cout << "," << dq[t];
-				}
-			}
-			cout << "]\n";
-		}
-
+bool compare(Data first, Data second) {
+	if (first.end < second.end) return true;
+	else if (first.end > second.end) return false;
+	else {
+		if (first.start < second.start) return true;
+		else return false;
 	}
-
-	return 0;
 }
 
 
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-// 기존에  R 명령의 구현에서 임시 덱을 두고 현재 덱의 상태에서 반복문을 통해 하나씩 원소를 역순으로 가져오는 방식으로 실제로 덱을 뒤집
-// but 이 경우 명령어 길이가 10000, 배열 내부 정수 개수가 10000 만 되어도 반복문 연산이 1억번이 되어 시간초과 발생
+	cin >> N;
 
-// 따라서 flag 변수를 두어 시작 부분이 오른쪽인지, 왼쪽인지만 판단 후 변수에 저장을 함으로써 위의 반복문을 생략
+	vector<Data> dataV;
 
-// 덱을 물리적으로 뒤집는게 아닌 논리적으로 뒤집는 발상을 하는 것이 중요했다
+	for (int i = 0; i < N; i++) {
+		int x, y;
+		cin >> x >> y;
+
+		dataV.push_back(Data{ x,y });
+	}
+
+	sort(dataV.begin(), dataV.end(), compare);  // 끝나는 시간이 빠른 순 정렬, 같다면 시작 시간 빠른 순 정렬
+
+	/*for (int i = 0; i < N; i++) {
+		cout << dataV[i].start << " " << dataV[i].end << "\n";
+	}*/
+
+	int answer = 1;
+	int currentTime = dataV[0].end;
+
+	for (int i = 1; i < N; i++) {
+		if (currentTime > dataV[i].start) continue;
+
+		answer++;
+		currentTime = dataV[i].end;
+	}
+
+	cout << answer;
+
+	return 0;
+}
