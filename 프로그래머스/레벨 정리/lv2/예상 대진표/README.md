9cb3583036db0dc430f6f07457b62ba5606e05fa
한창희 <nimok97@gmail.com>
2022-10-08 23:05:58 +0900

    C++ / 프로그래머스 - 예상 대진표 lv2

---
+++ 프로그래머스/레벨 정리/lv2/예상 대진표/README.md
@@ -0,0 +1,9 @@
+문제 : 예상 대진표
+<br>
+링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12985
+<br>
+레벨 : 2
+
+---
+
+- 이기고 올라가면서 새로 받은 번호가 같아지는 순간을 찾기
---
+++ 프로그래머스/레벨 정리/lv2/예상 대진표/예상 대진표.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int a, int b)
+{
+	int answer = 0;
+
+	while (a != b)
+	{ // 같다면 이전에 둘이 붙었다는 뜻
+
+		if (a % 2 == 0)
+			a /= 2;
+		else
+			a = a / 2 + 1;
+
+		if (b % 2 == 0)
+			b /= 2;
+		else
+			b = b / 2 + 1;
+
+		answer++;
+	}
+
+	return answer;
+}
